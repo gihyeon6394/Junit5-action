c65e1d926a7bd549800f14cb362e2d76171272aa
kgh <gihyeon6394@gmail.com>
2023-06-16 12:08:29 +0900

    inital commit

---
+++ src/test/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void test1(){
+        assertEquals(1, 1);
+    }
+    
+}
